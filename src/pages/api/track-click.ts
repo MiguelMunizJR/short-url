e88d58078a4487d0a502857ff3288f578f32e5e3
MiguelMunizJR <miguelmunizb@outlook.com>
2025-04-04 18:26:46 -0600

    click tracker completed

--- src/pages/api/track-click.ts
+++ src/pages/api/track-click.ts
@@ -2,24 +2,32 @@ import type { APIRoute } from "astro";
 import { db, eq, shortendURLTable } from "astro:db";
 
 export const POST: APIRoute = async ({ request }) => {
-  try {
-    const { hashURL, clicks } = await request.json();
+  const { hashURL } = await request.json();
 
-    if (!hashURL || !clicks === undefined) {
-      return new Response("Short URL or clicks are required", { status: 400 });
+  if (!hashURL) {
+    return new Response("Short URL or clicks are required", { status: 400 });
+  }
+
+  try {
+    //? Compobamos que exista
+    const [existingEntry] = await db
+      .select()
+      .from(shortendURLTable)
+      .where(eq(shortendURLTable.hash_url, hashURL));
+
+    if (!existingEntry) {
+      return new Response("URL not found", { status: 404 });
     }
 
-    //? Actualizar el clickCount
     const res = await db
       .update(shortendURLTable)
-      .set({ clicks: clicks + 1 })
+      .set({ clicks: existingEntry.clicks + 1 })
       .where(eq(shortendURLTable.hash_url, hashURL));
 
     if (res?.affectedRows === 0) {
       return new Response("Click not tracked", { status: 404 });
     }
 
-    console.log(res);
     return new Response("Click tracked", { status: 200 });
   } catch (error) {
     console.error("Error tracking click:", error);
